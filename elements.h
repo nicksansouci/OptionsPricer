4f2aeb375033eaf45f278a4cba97b711e06dc071
nicksansouci <nicksansouci@gmail.com>
2022-05-17 17:55:44 -0700

    Initial commit

---
+++ elements.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+class compoundInterest
+{
+public:
+	double principal;
+	double interestrate;
+	int periods;
+	compoundInterest() : principal{}, interestrate{}, periods{} {}
+	void calculate_interest();
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "elements.h"
+#include <iostream>
+
+
+
+
+int main()
+{
+	compoundInterest test1;
+	test1.calculate_interest();
+}
